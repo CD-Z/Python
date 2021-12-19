6b634f33c78cd9d4470a259203b47835a74e8809
Patrick Loser <p_loser_Austria@gmx.at>
2021-12-19 19:57:52 +0100

    started a bit with python

--- private/pythonProject/tkintertest3.py
+++ private/pythonProject/tkintertest3.py
@@ -15,10 +15,17 @@ class Ball:
     def __init__(self, canvas, color):
         self.canvas = canvas
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
+
         self.canvas.move(self.id, 245, 100)
 
+    def get_x(self):
+        return self.canvas.coords(self.id)[1]
+
+    def get_y(self):
+        return self.canvas.coords(self.id)[3]
+
     def draw(self):
-        if (self.canvas.coords(self.id)[3]>10):
+        if (self.get_y()>10):
             self.canvas.move(self.id, 0, -1)
         else:
             self.canvas.move(self.id, 1, 0)
