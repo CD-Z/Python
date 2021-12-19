e9a4f6e306a901bf89efca1416254b8cfb286644
Patrick Loser <p_loser_Austria@gmx.at>
2021-12-19 19:53:42 +0100

    started a bit with python

--- private/pythonProject/tkintertest.py
+++ private/pythonProject/tkintertest.py
@@ -3,7 +3,7 @@ from tkinter import *
 
 canvas_width = 500
 canvas_height = 150
-write_pos = 150
+x =10
 
 
 def paint(event):
@@ -13,18 +13,25 @@ def paint(event):
     x2, y2 = (event.x + 10), (event.y + 10)
     w.create_oval(x1, y1, x2, y2, fill=python_green)
 
-
 master = Tk()
 master.title("Painting using Ovals")
 w = Canvas(master,
-           width=canvas_width,
-           height=canvas_height)
+               width=canvas_width,
+               height=canvas_height)
 w.pack(expand=YES, fill=BOTH)
 master.bind("<Button-1>", paint)
-
-
-message = Label(master, text="Press and Drag the mouse to draw")
 Button(master, text="Hello").pack(side=BOTTOM)
+message = Label(master, text="Press and Drag the mouse to draw")
 message.pack(side=BOTTOM)
 
+
+w.create_oval(x, 10, x + 10, 20, fill="#348029")
+
+x = x +1
+
+
+
+
+master.update()
 master.mainloop()
+
---
+++ private/pythonProject/tkintertest3.py
@@ -0,0 +1,30 @@
+from tkinter import *
+import random
+import time
+
+tk = Tk()
+tk.title = "Game"
+tk.resizable(0, 0)
+tk.wm_attributes("-topmost", 1)
+
+canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
+canvas.pack()
+
+
+class Ball:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
+        self.canvas.move(self.id, 245, 100)
+
+    def draw(self):
+        if (self.canvas.coords(self.id)[3]>10):
+            self.canvas.move(self.id, 0, -1)
+        else:
+            self.canvas.move(self.id, 1, 0)
+        self.canvas.after(10, self.draw)  # (time_delay, method_to_execute)
+
+
+ball = Ball(canvas, "red")
+ball.draw()  # Changed per Bryan Oakley's comment
+tk.mainloop()
