0520cb2d028335d8de4632f9f825a4f1d04d8c5a
Patrick-Loser <p_loser_Austria@gmx.at>
2021-12-21 16:55:22 +0100

    it just works

--- private/pythonProject/tkintertest.py
+++ private/pythonProject/tkintertest.py
@@ -1,37 +1,32 @@
 from tkinter import *
 
-
 canvas_width = 500
 canvas_height = 150
-x =10
+x = 10
 
 
 def paint(event):
     print(event)
     python_green = "#476042"
     x1, y1 = (event.x - 10), (event.y - 10)
-    x2, y2 = (event.x + 10), (event.y + 10)
+    x2, y2 = (event.x + 20), (event.y + 10)
     w.create_oval(x1, y1, x2, y2, fill=python_green)
+
 
 master = Tk()
 master.title("Painting using Ovals")
 w = Canvas(master,
-               width=canvas_width,
-               height=canvas_height)
+           width=canvas_width,
+           height=canvas_height)
 w.pack(expand=YES, fill=BOTH)
 master.bind("<Button-1>", paint)
 Button(master, text="Hello").pack(side=BOTTOM)
 message = Label(master, text="Press and Drag the mouse to draw")
 message.pack(side=BOTTOM)
 
-
 w.create_oval(x, 10, x + 10, 20, fill="#348029")
 
-x = x +1
-
-
-
+x = x + 1
 
 master.update()
 master.mainloop()
-
--- private/pythonProject/tkintertest3.py
+++ private/pythonProject/tkintertest3.py
@@ -2,21 +2,19 @@ from tkinter import *
 import random
 import time
 
-tk = Tk()
-tk.title = "Game"
-tk.resizable(0, 0)
-tk.wm_attributes("-topmost", 1)
+# tk.resizable(0, 0)
+# tk.wm_attributes("-topmost", 1)
 
-canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
-canvas.pack()
+
+def create_ball(event):
+    print(event)
+    master.balls.add(Ball(master.tk.canvas, "#e67921", event.x, event.y))
 
 
 class Ball:
-    def __init__(self, canvas, color):
+    def __init__(self, canvas, color, x, y):
         self.canvas = canvas
-        self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
-
-        self.canvas.move(self.id, 245, 100)
+        self.id = canvas.create_oval(x, y, x + 15, y + 15, fill=color)
 
     def get_x(self):
         return self.canvas.coords(self.id)[1]
@@ -25,13 +23,34 @@ class Ball:
         return self.canvas.coords(self.id)[3]
 
     def draw(self):
-        if (self.get_y()>10):
+        if self.get_y() > 10:
             self.canvas.move(self.id, 0, -1)
         else:
             self.canvas.move(self.id, 1, 0)
-        self.canvas.after(10, self.draw)  # (time_delay, method_to_execute)
+        #self.canvas.after(10, self.draw)
 
 
-ball = Ball(canvas, "red")
-ball.draw()  # Changed per Bryan Oakley's comment
-tk.mainloop()
+class Window:
+    def __init__(self):
+        self.tk = Tk()
+        self.tk.title = "Game"
+        self.tk.canvas = Canvas(self.tk, width=500, height=400, bd=0, highlightthickness=0, borderwidth=2, relief="solid")
+        self.balls = set()
+
+        self.tk.canvas.pack(expand=True, fill=BOTH)
+        self.tk.grid_columnconfigure(0, weight=1)
+        self.tk.grid_rowconfigure(0, weight=1)
+        self.tk.bind("<Button-1>", create_ball)
+        self.ball = Ball(self.tk.canvas, "red", 10, 100)
+        print("KFDLKJÖF")
+
+    def update(self):
+        self.ball.draw()
+        for ball in self.balls:
+            ball.draw()
+        self.tk.canvas.after(10, self.update)
+
+
+master = Window()
+master.update()
+master.tk.mainloop()
