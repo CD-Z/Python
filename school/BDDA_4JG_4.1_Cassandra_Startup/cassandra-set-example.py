b68531ce5150feeee15e5e3102537ed299081a4c
Patrick-Loser <p_loser_Austria@gmx.at>
2022-03-04 11:41:33 +0100

    had school

--- school/BDDA_4JG_4.1_Cassandra_Startup/cassandra-set-example.py
+++ school/BDDA_4JG_4.1_Cassandra_Startup/cassandra-set-example.py
@@ -1,16 +1,16 @@
 from cassandra.cluster import Cluster
 
 cluster = Cluster(['10.115.2.20'], port=8042)
-session = cluster.connect('store', wait_for_all_pools=True)
-session.execute('USE store')
+session = cluster.connect('school', wait_for_all_pools=True)
+session.execute('USE school')
 
 rows = session.execute('SELECT * FROM student')
 
 
 def start():
     for row in rows:
-        print(f"userid: {row.userid}; Item-count: {row.name}; contact: {row.emails[0]}")
-        print(f"                                        {row.emails[1]}")
+        print(f"userid: {row.userid}; User-Name: {row.name}; contact: {row.e_mails[0]}")
+        print(f"                                       {row.e_mails[1]}")
 
 
 if __name__ == '__main__':
